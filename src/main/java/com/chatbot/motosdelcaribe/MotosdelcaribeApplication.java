06647ffb2dd5e0084c8d53fa4e8817fa9dfc7dd9
Barrera <sebbarrera0@gmail.com>
2026-04-26 09:04:39 -0500

    inicializacion del proyecto

---
+++ src/main/java/com/chatbot/motosdelcaribe/MotosdelcaribeApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot.motosdelcaribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotosdelcaribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotosdelcaribeApplication.class, args);
+	}
+
+}
